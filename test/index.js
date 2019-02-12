30331860e89d437a62ec129a4d01c3e76f4bfd68
lukequ <lukequ@webank.com>
2019-02-12 20:58:51 +0800

    add extension type and modify docs

--- test/index.js
+++ test/index.js
@@ -1,46 +1,87 @@
 var resolveLogic = require('../lib/Logic.js');
+const expect = require('chai').expect;
 var requirement = {
-    nodeType: "logic",
-    logicType: "&&",
+    operator: "&&",
     left: {
-        nodeType: "logic",
-        logicType: "||",
+        operator: "&&",
         left: {
-            nodeType: "requirement",
-            requireType: "enum",
+            operator: "in",
             key: "month",
-            range: [0, 45]
+            range: [1, 12]
         },
         right: {
-            nodeType: "requirement",
-            requireType: ">",
-            key: "day",
-            compareValue: 4
+            operator: "bool",
+            value: "false"
         }
     },
     right: {
-        nodeType: "logic",
-        logicType: "||",
+        operator: "||",
         left: {
-            nodeType: "requirement",
-            requireType: "regex",
+            operator: "match",
             regExp: "^\\S{1,30}$",
-            key: "year"
+            key: "text"
         },
         right: {
-            nodeType: "requirement",
-            requireType: "[]",
+            operator: "[]",
             key: "hour",
             range: [0, 10]
         }
-
     }
 };
-var person = {
-    year: "fdafdffdafdffdafdffdafdf",
-    month: 45,
+var requirement1 = {
+    operator: "&&",
+    left: {
+        operator: "&&",
+        left: {
+            operator: ">",
+            key: "month",
+            value: 30
+        },
+        right: {
+            operator: "<",
+            key: "hour",
+            value: 8
+        }
+    },
+    right: {
+        operator: "||",
+        left: {
+            operator: "<=",
+            key: "day",
+            value: 2
+        },
+        right: {
+            operator: ">=",
+            key: "year",
+            value: 20
+        }
+    }
+};
+var requirement2 = {
+    operator: "=",
+    key: "year",
+    value: 10
+};
+var date = {
+    text: "fdafdffdafdffdafdffdafds",
+    year: 10,
+    month: 12,
     day: 3,
     hour: 6
 };
-var result  = resolveLogic(requirement, person, false);
+var result  = resolveLogic(requirement, date, false);
 console.log('result:  '+ result);
+
+describe("logic", function () {
+    describe("requirement", function () {
+        it("resolve match", function () {
+            expect(resolveLogic(requirement, date, false)).to.equal(true);
+        });
+        it("resolve compare", function () {
+            expect(resolveLogic(requirement1, date, false)).to.equal(false);
+        })
+        it("resolve equal", function () {
+            expect(resolveLogic(requirement2, date, false)).to.equal(true);
+        })
+    })
+})
